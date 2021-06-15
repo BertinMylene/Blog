c282cc5630a3b3f8148c12cd39668a9e272e7091
Mylène <bertinmylene03@gmail.com>
2021-06-15 17:23:50 +0200

    Commit 1

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php
+define('ROOT', dirname(__DIR__));
+
+require ROOT . '/App/App.php';
+require_once ROOT . '/vendor/autoload.php';
+
+App::load();
+
+
+if (isset($_GET['p'])) {
+    $p = $_GET['p'];
+} else {
+    $p = 'posts.index';
+}
+
+$p = explode('.', $p);
+
+if ($p[0] === 'admin') {
+    $controllerName = '\App\Controller\\' . ucfirst($p[0]) . '\\' . ucfirst($p[1]) . 'Controller';
+    $action = $p[2];
+} else {
+    $controllerName = '\App\Controller\\' . ucfirst($p[0]) . 'Controller';
+    $action = $p[1];
+}
+
+$controller = new $controllerName();
+$controller->$action();
